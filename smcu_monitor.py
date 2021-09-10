cddc3fe707b3aba927d5774703b3b9c5a797853c
Elena Kotlyarova <elenakotlyrova@gmail.com>
2021-09-10 11:37:57 +0300

    Add payload data size calculation

--- smcu_monitor.py
+++ smcu_monitor.py
@@ -1,6 +1,6 @@
 import socket
 from socket import AF_INET, SOCK_DGRAM
-from struct import *
+from struct import unpack, calcsize
 
 # Create a socket object
 s = socket.socket(family=AF_INET, type=SOCK_DGRAM, proto=0)
@@ -13,43 +13,22 @@ UDP_PORT = 20817
 
 s.bind((host, UDP_PORT))
 
-# The UDP diagram payload has the following data fields sizes
-
-# Constant '0xACDC'
-MAGIC_SZ = 2
-
-# Constant '0x0100'
-VERSION_SZ = 2
-
-# Angular Z1 position
-APOSZ1_SZ = 4
-
-# Angular Z2 position
-APOSZ2_SZ = 4
-
-# Angular Z3 position
-APOSZ3_SZ = 4
-
-# Linear X position
-LPOSX_SZ = 4
-
-# Linear Y position
-LPOSY_SZ = 4
-
-# Data fields offsets
-APOSZ1_OFF = 4
-APOSZ2_OFF = 8
-APOSZ3_OFF = 12
-LPOSX_OFF = 16
-LPOSY_OFF = 20
+# The UDP diagram payload has data fields described in struct format
+payload_fmt = '>HH5i'
+size = calcsize(payload_fmt)
 
 # Receive a new position updates broadcast protocol packet from SMCU model
-while packet := s.recv(24):
+while packet := s.recv(size):
 
     # Return a tuple containing integers. The integers refer to positions
-    pos = unpack('>5i', packet[4:25])
+    pos = unpack(payload_fmt, packet)
 
-    print(
-        f"APOSZ1: {pos[0]}, APOSZ2: {pos[1]}, APOSZ3: {pos[2]}, \
-LPOSX: {pos[3]}, LPOSY: {pos[4]}"
-        )
+    magic, version, aposz1, aposz2, aposz3, lposx, lposy = pos
+
+    # MAGIC is a constant '0xACDC', VERSION is a constant '0x0100'
+    if magic == 44252 and version == 256:
+
+        print(
+            f"APOSZ1: {aposz1}, APOSZ2: {aposz1}, APOSZ3: {aposz3}, \
+LPOSX: {lposx}, LPOSY: {lposy}"
+            )
