ded425ba81d800117d6f81a1fd7ac9c8c023eb80
Elena Kotlyarova <elenakotlyrova@gmail.com>
2021-10-17 20:53:11 +0300

    Add KeyboardInterrupt exception handling

--- smcu_monitor.py
+++ smcu_monitor.py
@@ -1,35 +1,42 @@
-import socket
-from socket import AF_INET, SOCK_DGRAM
+#!/usr/bin/env python
+
+"""
+Receives position updates via UDP
+"""
+
+from socket import socket, AF_INET, SOCK_DGRAM
 from struct import unpack, calcsize
 
 # Listen address
-host = "127.0.0.1"
+DEFAULT_HOST = "127.0.0.1"
 
 # Reserve a port for UDP protocol
 UDP_PORT = 20817
 
 # Create a socket object
-s = socket.socket(family=AF_INET, type=SOCK_DGRAM, proto=0)
+s = socket(family=AF_INET, type=SOCK_DGRAM, proto=0)
 
-s.bind((host, UDP_PORT))
+s.bind((DEFAULT_HOST, UDP_PORT))
 
 # The UDP diagram payload has data fields described in struct format
-payload_fmt = '>HH5i'
-size = calcsize(payload_fmt)
+PAYLOAD_FMT = ">HH5i"
+size = calcsize(PAYLOAD_FMT)
 
-# Receive a new position updates broadcast protocol packet from SMCU model
-while packet := s.recv(size):
+try:
+    # Receive a new position updates broadcast protocol packet from SMCU model
+    while packet := s.recv(size):
 
-    # Return a tuple containing integers. The integers refer to positions
-    pos = unpack(payload_fmt, packet)
+        # Return a tuple containing integers. The integers refer to positions
+        pos = unpack(PAYLOAD_FMT, packet)
 
-    magic, version, aposz1, aposz2, aposz3, lposx, lposy = pos
+        magic, version, aposz1, aposz2, aposz3, lposx, lposy = pos
 
-    # MAGIC is a constant '0xACDC', VERSION is a constant '0x0100'
-    if magic != 0xACDC and version != 0x0100:
-        continue
-
-    print(
-        f"APOSZ1: {aposz1}, APOSZ2: {aposz2}, APOSZ3: {aposz3}, \
+        # MAGIC is a constant '0xACDC', VERSION is a constant '0x0100'
+        if magic != 0xACDC and version != 0x0100:
+            continue
+        print(
+            f"APOSZ1: {aposz1}, APOSZ2: {aposz2}, APOSZ3: {aposz3}, \
 LPOSX: {lposx}, LPOSY: {lposy}"
         )
+except KeyboardInterrupt:
+    print("\nStopped by user")
